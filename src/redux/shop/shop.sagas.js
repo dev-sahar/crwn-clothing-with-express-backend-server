23ed092caa4a450d03ef78227bbc8074802e4d67
Sahar S <dev_sahar@hotmail.com>
2020-04-12 17:25:16 +0800

    added redux-saga, created shop sagas and user and cart sagas for sign in, sign in with google, and sign up

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,37 @@
+import { takeLatest, call, put, all } from "redux-saga/effects";
+
+import { firestore, convertCollectinsSnapshotToMap } from  "../../firebase/firebase.utils";
+import { fetchCollectionsSuccess, fetchCollectionFailure } from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+
+    try {
+        const collectionRef = firestore.collection("collections");
+        const snapshot = yield collectionRef.get();
+        const collectionsMap = yield call(
+            convertCollectinsSnapshotToMap, 
+            snapshot
+        );
+        yield put(fetchCollectionsSuccess(collectionsMap));
+    } catch (error) {
+        yield put(fetchCollectionFailure(error.message));
+    }
+    
+}
+
+export function* fetchCollectionsStart() {
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START, 
+        fetchCollectionsAsync
+    );
+}
+
+export function* shopSagas() {
+    yield all(
+        [
+            call(fetchCollectionsStart)
+        ]
+    )
+}
